311d4c07f3a2ae021afdb5524767a0db08107bca
Pear1yCode <gh_wns@naver.com>
2024-12-16 17:23:50 +0900

    feat: Signup Api/2024-12-16

--- src/main/java/com/ohgiraffers/ukki/user/model/dao/SignupMapper.java
+++ src/main/java/com/ohgiraffers/ukki/user/model/dao/SignupMapper.java
@@ -1,4 +1,8 @@
 package com.ohgiraffers.ukki.user.model.dao;
 
-public class SignupMapper {
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SignupMapper {
+    int signupId(String userId);
 }
