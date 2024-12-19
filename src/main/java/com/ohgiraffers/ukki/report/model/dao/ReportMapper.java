c44c30858f7befc5e56e994a8bad631073db60dd
Passbob <gudjtr097@gmail.com>
2024-12-19 12:38:05 +0900

    feat: Report List/2024-12-19

--- src/main/java/com/ohgiraffers/ukki/report/model/dao/ReportMapper.java
+++ src/main/java/com/ohgiraffers/ukki/report/model/dao/ReportMapper.java
@@ -1,4 +1,11 @@
 package com.ohgiraffers.ukki.report.model.dao;
 
+import com.ohgiraffers.ukki.report.model.dto.ReportDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
 public interface ReportMapper {
+    List<ReportDTO> findReportList(int userNo);
 }
