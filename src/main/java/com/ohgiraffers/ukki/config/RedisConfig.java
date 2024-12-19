20bfba6dbddcc455620b0212b4684eb42bc48db4
Pear1yCode <gh_wns@naver.com>
2024-12-19 15:38:00 +0900

    feat: Signup API/2024-12-19

--- src/main/java/com/ohgiraffers/ukki/config/RedisConfig.java
+++ src/main/java/com/ohgiraffers/ukki/config/RedisConfig.java
@@ -1,31 +1,36 @@
 package com.ohgiraffers.ukki.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    // yml에 넘기려고 했는데 yml에서 컴파일 에러가 떠서 config 사용
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
     @Bean
-    public RedisTemplate<String, String> redisTemplate() { // JedisConnectionFactory 설정 -> yml에 하려고했는데 yml에 컴파일에러떠서 넘김(보안상으론 부족할듯..)
-        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-        jedisConnectionFactory.setHostName("localhost"); // Redis 서버 호스트 이름 ?
-        jedisConnectionFactory.setPort(6379);            // Redis 서버 포트 -> 기본값이 6379
-        jedisConnectionFactory.setPassword("ukki");      // Redis 비번
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
+        factory.setPassword(redisPassword);  // 비밀번호 설정
+        return factory;
+    }
 
-        // 없으면 에러남 (초기화?)
-        jedisConnectionFactory.start();
-
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-
-        return template;
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
     }
 }
