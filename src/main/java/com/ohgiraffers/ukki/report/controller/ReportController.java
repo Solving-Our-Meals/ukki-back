ae86e1575879f8e331c8c7fd8225c4250e02ed3a
Passbob <gudjtr097@gmail.com>
2024-12-23 15:07:32 +0900

    feat: Report Update And Delete/2024-12-23

--- src/main/java/com/ohgiraffers/ukki/report/controller/ReportController.java
+++ src/main/java/com/ohgiraffers/ukki/report/controller/ReportController.java
@@ -4,16 +4,9 @@ import com.ohgiraffers.ukki.report.model.dto.ReportDTO;
 import com.ohgiraffers.ukki.report.model.dto.ReportListDTO;
 import com.ohgiraffers.ukki.report.model.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -93,36 +86,5 @@ public class ReportController {
         responseMap.put("message", message);
 
         return ResponseEntity.ok(responseMap);
-    }
-    public int fileController(MultipartFile file, String fileName){
-
-        //                파일이름에서 확장자 뽑아내기
-        String fileExtension = "";
-        String originalFileName =file.getOriginalFilename();
-        int dotIndex = originalFileName.lastIndexOf('.');
-        if (dotIndex > 0 && dotIndex < originalFileName.length() - 1){
-            fileExtension = originalFileName.substring(dotIndex);
-        }
-        //                최종파일 이름에 확장자 추가
-        String fileSetName = fileName+fileExtension;
-        try{
-//                경로설정
-            Path networkPath = Paths.get("\\\\192.168.0.138\\ukki_nas\\inquiry");
-            if(!Files.exists(networkPath)){
-                Files.createDirectories(networkPath);
-            }
-
-//                파일 저장 - 경로에 파일이름 붙이기
-//                StandardCopyOption.REPLACE_EXISTING은 대상 경로에 동일한 이름의 파일이 이미 존재할 경우 그 카일을 덮어쓰도록하는 옵션
-            Path filePath = networkPath.resolve(fileSetName);
-            Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-
-            System.out.println(file.getInputStream());
-            System.out.println("파일 저장 성공: " + fileSetName);
-            return 2;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return 1;
-        }
     }
 }
