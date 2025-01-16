a9157b0339f38dcee3df6b4a43c4ba1464d898f6
Passbob <gudjtr097@gmail.com>
2025-01-17 02:46:19 +0900

    hotfix: Qr Image Save/2025-01-17

--- src/main/java/com/ohgiraffers/ukki/qr/model/dao/QrMapper.java
+++ src/main/java/com/ohgiraffers/ukki/qr/model/dao/QrMapper.java
@@ -1,11 +1,11 @@
 package com.ohgiraffers.ukki.qr.model.dao;
 
-import com.ohgiraffers.ukki.qr.model.dto.QrConfirmDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface QrMapper {
-    QrConfirmDTO qrConfirmation(String qr);
 
-    int qrConfirmSuccess(String qr);
+    String resStoreUserName(String qr);
+
+    void editQrConfirmRes(String qr);
 }
--- src/main/java/com/ohgiraffers/ukki/qr/model/service/QrService.java
+++ src/main/java/com/ohgiraffers/ukki/qr/model/service/QrService.java
@@ -1,7 +1,11 @@
 package com.ohgiraffers.ukki.qr.model.service;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.ohgiraffers.ukki.qr.model.dao.QrMapper;
-import com.ohgiraffers.ukki.qr.model.dto.QrConfirmDTO;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.util.ByteArrayDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +16,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class QrService {
+
+    //    private final String SHARED_FOLDER = "\\\\192.168.0.138\\ukki_nas\\qr";
+    private final String SHARED_FOLDER = "C:\\Users\\admin\\Desktop\\ukkiImg";
 
     @Autowired
     private JavaMailSender mailSender;
@@ -57,11 +70,56 @@ public class QrService {
         }
     }
 
-    public QrConfirmDTO qrConfirmation(String qr) {
-        return qrMapper.qrConfirmation(qr);
+    public int qrConfirmation(String qr, String userId) {
+//    qr로 가게 사장 아이디 불러오기
+      String storeUserName = qrMapper.resStoreUserName(qr);
+        System.out.println(storeUserName);
+
+        if(storeUserName.equals(userId)) {
+            return 1;
+        }else {
+            return 0;
+        }
     }
 
-    public int qrConfirmSuccess(String qr) {
-        return qrMapper.qrConfirmSuccess(qr);
+    public String qrCertificate() throws WriterException {
+        System.out.println("실행");
+        String randomCode = String.valueOf(UUID.randomUUID());
+        String qrName = "resQr"+randomCode;
+
+
+        System.out.println(qrName);
+        int width = 200;
+        int height = 200;
+        String url = "http://localhost:3000/qr/"+qrName;
+
+        String filePath = SHARED_FOLDER + "/"+ qrName+".png";
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(url, BarcodeFormat.QR_CODE, width, height);
+        File outputFile = new File(filePath);
+
+//        경로가 존재하는지 파악하기 위함
+        File parentDir = outputFile.getParentFile();
+
+        if (!parentDir.exists()) {
+            parentDir.mkdirs(); // 경로가 존재하지 않으면 생성
+        }
+
+        try(FileOutputStream fos = new FileOutputStream(outputFile)){
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(bitMatrix, "png", baos);
+            fos.write(baos.toByteArray());
+
+
+            return qrName;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void editQrConfirmRes(String qr) {
+        qrMapper.editQrConfirmRes(qr);
     }
 }
