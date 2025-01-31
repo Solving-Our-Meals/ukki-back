001f80f0312e5bb9ff289e4e787af0e05cad1289
Passbob <gudjtr097@gmail.com>
2025-01-31 17:19:33 +0900

    fix: URL Fix /2025-01-31

--- src/main/java/com/ohgiraffers/ukki/config/WebConfig.java
+++ src/main/java/com/ohgiraffers/ukki/config/WebConfig.java
@@ -29,8 +29,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                    // .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost", "http://localhost:80")
-           .allowedOrigins("http://3.39.119.249:3000", "http://3.39.119.249", "http://3.39.119.249:80", "http://3.39.119.249:8080")
+//                     .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://localhost", "http://localhost:80")
+//           .allowedOrigins("http://3.39.119.249:3000", "http://3.39.119.249", "http://3.39.119.249:80", "http://3.39.119.249:8080")
+                   .allowedOrigins("http://43.200.92.61:3000", "http://43.200.92.61", "http://43.200.92.61:80", "http://43.200.92.61:8080")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true)
